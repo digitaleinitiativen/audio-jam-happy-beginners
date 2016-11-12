3537ad165790931e6502943f4ab625da15646339
joemoe <johannes@crate.io>
2016-11-12 10:45:35 +0100

    make tune up smoother

--- sequencer.js
+++ sequencer.js
@@ -5,14 +5,15 @@ var osc = context.createOscillator();
 osc.connect(context.destination);
 
 var frequenceStart = 200;
-var frequenceStep = 20;
+var frequenceStep = 5;
 var frequenceCurrent = frequenceStart;
 
 window.setInterval(function() {
 	osc.frequency.value = frequenceCurrent;
 	frequenceCurrent += frequenceStep;
 	console.log(osc.frequency.value);
-}, 200);
+	if(frequenceCurrent == 1000) frequenceCurrent = frequenceStart;
+}, 20);
 
 
 osc.start(0);
