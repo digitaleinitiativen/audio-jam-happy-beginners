cb82efb1e1cd48e139f7b3b05e0880831059f6c2
joemoe <johannes@crate.io>
2016-11-12 10:40:32 +0100

    random tone per second

--- sequencer.js
+++ sequencer.js
@@ -4,4 +4,11 @@ var osc = context.createOscillator();
 
 osc.connect(context.destination);
 
+
+window.setInterval(function() {
+	osc.frequency.value = 200 + Math.random() * 5000;
+	console.log(osc.frequency.value);
+}, 200);
+
+
 osc.start(0);
